66e5951f859ef5ccc08d61500776c8e6473287b0
Yazhong Liu <yorkiefixer@gmail.com>
2018-01-17 14:39:33 +0800

    crypto: init module and add randomBytes() (#7)

--- src/js/crypto.js
+++ src/js/crypto.js
@@ -1,1 +1,11 @@
-module.exports = {};
+// var _randomBytes = native.randomBytes;
+var _randomBytesSync = native.randomBytesSync;
+
+exports.randomBytes = function(size, callback) {
+  var bytes = _randomBytesSync(size);
+  var buf = new Buffer(bytes);
+  if (typeof callback === 'function') {
+    callback(null, buf);
+  }
+  return buf;
+};
