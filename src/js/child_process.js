1943bd70d1c89aae49213b84cfcab1df4805c2cf
Yorkie Liu <yorkiefixer@gmail.com>
2018-01-24 15:18:34 +0800

    child_process: add .exec()

--- src/js/child_process.js
+++ src/js/child_process.js
@@ -3,6 +3,7 @@
 var Process = native.Process;
 var Pipe = require('pipe_wrap').Pipe;
 var EventEmitter = require('events').EventEmitter;
+var StringDecoder = require('string_decoder').StringDecoder;
 var util = require('util');
 var net = require('net');
 
@@ -158,6 +159,9 @@ ChildProcess.prototype.spawn = function(options) {
 
   for (i = 0; i < stdio.length; i++)
     this.stdio.push(stdio[i].socket === undefined ? null : stdio[i].socket);
+
+  // Add .send() method and start listening for IPC data
+  if (ipc !== undefined) setupChannel(this, ipc);
 
   return err;
 };
@@ -380,17 +384,139 @@ function maybeClose(subprocess) {
   }
 }
 
-exports.ChildProcess = ChildProcess;
+function Control(channel) {
+  EventEmitter.call(this);
+  this.channel = channel;
+  this.refs = 0;
+}
+util.inherits(Control, EventEmitter);
 
-exports.fork = function(modulePath /*, args, options*/) {
-  // TODO
+Control.prototype.ref = function() {
+  if (++this.refs === 1) {
+    this.channel.ref();
+  }
 };
 
-exports.exec = function(command /*, options, callback*/) {
-  // TODO
+Control.prototype.unref = function() {
+  if (--this.refs === 0) {
+    this.channel.unref();
+    this.emit('unref');
+  }
 };
 
-exports.spawn = function(/*file, args, options*/) {
+function setupChannel(target, channel) {
+  target.channel = channel;
+
+  // _channel can be deprecated in version 8
+  Object.defineProperty(target, '_channel', {
+    get: function() {
+      return target.channel;
+    },
+    set: function(val) {
+      target.channel = val;      
+    },
+    enumerable: true
+  });
+
+  target._handleQueue = null;
+  target._pendingMessage = null;
+
+  var control = new Control(channel);
+  var decoder = new StringDecoder('utf8');
+  var jsonBuffer = '';
+  var pendingHandle = null;
+  channel.buffering = false;
+  channel.onread = function(nread, pool, recvHandle) {
+    // TODO
+  };
+
+  // object where socket lists will live
+  channel.sockets = { got: {}, send: {} };
+
+  // handlers will go through this
+  target.on('internalMessage', function(message, handle) {
+    // TODO
+  });
+
+  target.send = function(message, handle, options, callback) {
+    // TODO
+  };
+
+  target._send = function(message, handle, options, callback) {
+    // TODO
+  };
+
+  // connected will be set to false immediately when a disconnect() is
+  // requested, even though the channel might still be alive internally to
+  // process queued messages. The three states are distinguished as follows:
+  // - disconnect() never requested: channel is not null and connected
+  //   is true
+  // - disconnect() requested, messages in the queue: channel is not null
+  //   and connected is false
+  // - disconnect() requested, channel actually disconnected: channel is
+  //   null and connected is false
+  target.connected = true;
+
+  target.disconnect = function() {
+    if (!this.connected) {
+      this.emit('error', new Error('ERR_IPC_DISCONNECTED'));
+      return;
+    }
+
+    // Do not allow any new messages to be written.
+    this.connected = false;
+
+    // If there are no queued messages, disconnect immediately. Otherwise,
+    // postpone the disconnect so that it happens internally after the
+    // queue is flushed.
+    if (!this._handleQueue)
+      this._disconnect();
+  };
+
+  target._disconnect = function() {
+    // This marks the fact that the channel is actually disconnected.
+    this.channel = null;
+
+    if (this._pendingMessage)
+      closePendingHandle(this);
+
+    var fired = false;
+    function finish() {
+      if (fired) return;
+      fired = true;
+
+      channel.close();
+      target.emit('disconnect');
+    }
+
+    // If a message is being read, then wait for it to complete.
+    if (channel.buffering) {
+      this.once('message', finish);
+      this.once('internalMessage', finish);
+
+      return;
+    }
+
+    process.nextTick(finish);
+  };
+
+  function emit(event, message, handle) {
+    target.emit(event, message, handle);
+  }
+
+  function handleMessage(message, handle, internal) {
+    if (!target.channel)
+      return;
+
+    var eventName = (internal ? 'internalMessage' : 'message');
+    process.nextTick(emit, eventName, message, handle);
+  }
+
+  channel.readStart();
+  return control;
+}
+
+var spawn = exports.spawn = function(/*file, args, options*/) {
   var opts = normalizeSpawnArguments.apply(null, arguments);
   var options = opts.options;
   var child = new ChildProcess();
@@ -409,3 +535,236 @@ exports.spawn = function(/*file, args, options*/) {
 
   return child;
 };
+
+exports.fork = function(modulePath /*, args, options*/) {
+  // TODO
+};
+
+function normalizeExecArgs(command, options, callback) {
+  if (typeof options === 'function') {
+    callback = options;
+    options = undefined;
+  }
+
+  // Make a shallow copy so we don't clobber the user's options object.
+  options = Object.assign({}, options);
+  options.shell = typeof options.shell === 'string' ? options.shell : true;
+
+  return {
+    file: command,
+    options: options,
+    callback: callback
+  };
+}
+
+exports.exec = function(command /*, options, callback*/) {
+  var opts = normalizeExecArgs.apply(null, arguments);
+  return exports.execFile(opts.file,
+                          opts.options,
+                          opts.callback);
+};
+
+exports.execFile = function(file /*, args, options, callback*/) {
+  var args = [];
+  var callback;
+  var options = {
+    encoding: 'utf8',
+    timeout: 0,
+    maxBuffer: 200 * 1024,
+    killSignal: 'SIGTERM',
+    cwd: null,
+    env: null,
+    shell: false
+  };
+
+  // Parse the optional positional parameters.
+  var pos = 1;
+  if (pos < arguments.length && Array.isArray(arguments[pos])) {
+    args = arguments[pos++];
+  } else if (pos < arguments.length && arguments[pos] == null) {
+    pos++;
+  }
+
+  if (pos < arguments.length && typeof arguments[pos] === 'object') {
+    options = Object.assign(options, arguments[pos++]);
+  } else if (pos < arguments.length && arguments[pos] == null) {
+    pos++;
+  }
+
+  if (pos < arguments.length && typeof arguments[pos] === 'function') {
+    callback = arguments[pos++];
+  }
+
+  if (!callback && pos < arguments.length && arguments[pos] != null) {
+    throw new TypeError('Incorrect value of args option');
+  }
+
+  // Validate the timeout, if present.
+  validateTimeout(options.timeout);
+
+  // Validate maxBuffer, if present.
+  validateMaxBuffer(options.maxBuffer);
+
+  options.killSignal = options.killSignal;
+
+  var child = spawn(file, args, {
+    cwd: options.cwd,
+    env: options.env,
+    gid: options.gid,
+    uid: options.uid,
+    shell: options.shell,
+  });
+
+  var encoding;
+  var _stdout;
+  var _stderr;
+  if (options.encoding !== 'buffer') {
+    encoding = options.encoding;
+    _stdout = '';
+    _stderr = '';
+  } else {
+    _stdout = [];
+    _stderr = [];
+    encoding = null;
+  }
+  var stdoutLen = 0;
+  var stderrLen = 0;
+  var killed = false;
+  var exited = false;
+  var timeoutId;
+
+  var ex = null;
+  var cmd = file;
+
+  function exithandler(code, signal) {
+    if (exited) return;
+    exited = true;
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+    }
+
+    if (!callback) return;
+
+    // merge chunks
+    var stdout;
+    var stderr;
+    if (encoding) {
+      stdout = _stdout;
+      stderr = _stderr;
+    } else {
+      stdout = Buffer.concat(_stdout);
+      stderr = Buffer.concat(_stderr);
+    }
+
+    if (!ex && code === 0 && signal === null) {
+      callback(null, stdout, stderr);
+      return;
+    }
+
+    if (args.length !== 0)
+      cmd += ' ' + args.join(' ');
+
+    if (!ex) {
+      ex = new Error('Command failed: ' + cmd + '\n' + stderr);
+      ex.killed = child.killed || killed;
+      ex.code = code < 0 ? errname(code) : code;
+      ex.signal = signal;
+    }
+
+    ex.cmd = cmd;
+    callback(ex, stdout, stderr);
+  }
+
+  function errorhandler(e) {
+    ex = e;
+
+    if (child.stdout)
+      child.stdout.destroy();
+
+    if (child.stderr)
+      child.stderr.destroy();
+
+    exithandler();
+  }
+
+  function kill() {
+    if (child.stdout)
+      child.stdout.destroy();
+
+    if (child.stderr)
+      child.stderr.destroy();
+
+    killed = true;
+    try {
+      child.kill(options.killSignal);
+    } catch (e) {
+      ex = e;
+      exithandler();
+    }
+  }
+
+  if (options.timeout > 0) {
+    timeoutId = setTimeout(function delayedKill() {
+      kill();
+      timeoutId = null;
+    }, options.timeout);
+  }
+
+  if (child.stdout) {
+    if (encoding)
+      child.stdout.setEncoding(encoding);
+
+    child.stdout.on('data', function onChildStdout(chunk) {
+      stdoutLen += chunk.length;
+
+      if (stdoutLen > options.maxBuffer) {
+        ex = new Error('stdout maxBuffer exceeded');
+        kill();
+      } else {
+        if (encoding)
+          _stdout += chunk;
+        else
+          _stdout.push(chunk);
+      }
+    });
+  }
+
+  if (child.stderr) {
+    if (encoding)
+      child.stderr.setEncoding(encoding);
+
+    child.stderr.on('data', function onChildStderr(chunk) {
+      stderrLen += chunk.length;
+
+      if (stderrLen > options.maxBuffer) {
+        ex = new Error('stderr maxBuffer exceeded');
+        kill();
+      } else {
+        if (encoding)
+          _stderr += chunk;
+        else
+          _stderr.push(chunk);
+      }
+    });
+  }
+
+  child.addListener('close', exithandler);
+  child.addListener('error', errorhandler);
+  return child;
+};
+
+exports.ChildProcess = ChildProcess;
+
+function validateTimeout(timeout) {
+  if (timeout != null && timeout < 0) {
+    throw new TypeError('"timeout" must be an unsigned integer');
+  }
+}
+
+function validateMaxBuffer(maxBuffer) {
+  if (maxBuffer != null && !(typeof maxBuffer === 'number' && maxBuffer >= 0)) {
+    throw new TypeError('"maxBuffer" must be a positive number');
+  }
+}
