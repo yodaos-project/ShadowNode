e977de0e4a9954b23f40de3200b0cc2332a5c2e4
Yorkie Liu <yorkiefixer@gmail.com>
2018-07-12 16:37:35 +0800

    tty: implement the tty.isatty()

---
+++ src/modules/iotjs_module_tty.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <errno.h>
+#include <uv.h>
+#include "iotjs_def.h"
+
+JS_FUNCTION(Isatty) {
+  int fd = JS_GET_ARG(0, number);
+  int rc = isatty(fd);
+  if (rc == 0 && errno == EBADF) {
+    return JS_CREATE_ERROR(COMMON, "fd is invalid descriptor");
+  }
+  return jerry_create_boolean(rc);
+}
+
+jerry_value_t InitTty() {
+  jerry_value_t jtty = jerry_create_object();
+  iotjs_jval_set_method(jtty, IOTJS_MAGIC_STRING_ISATTY, Isatty);
+  return jtty;
+}
