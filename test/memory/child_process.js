4e40b012c8c65479cf698523b7b8d8b557f88a83
Yorkie Liu <yorkiefixer@gmail.com>
2018-07-12 20:44:43 +0800

    optimize the output of script

--- test/memory/child_process.js
+++ test/memory/child_process.js
@@ -7,9 +7,11 @@ var matrix = {
   startRss: 0,
   endRss: 0,
   samples: 0,
+  increasingCount: 0,
+  decreasingCount: 0,
 };
 
-var duration = process.argv[2] || 5000;
+var duration = process.argv[2] || 7000;
 var cycle = process.argv[3] || 0;
 
 (function main() {
@@ -22,9 +24,14 @@ var cycle = process.argv[3] || 0;
         matrix.startRss = curr;
       matrix.endRss = rss;
       matrix.samples += 1;
+      if (curr - rss > 0) {
+        matrix.increasingCount += 1;
+      } else if (curr - rss < 0) {
+        matrix.decreasingCount += 1;
+      }
     }
-    if (curr - rss > 0) {
-      console.log(`> total: ${curr}, increase: ${curr - rss}`);
+    if (curr - rss !== 0) {
+      console.log(`:: ${curr} + ${curr - rss} bytes`);
     }
     main();
   }, cycle);
@@ -32,11 +39,17 @@ var cycle = process.argv[3] || 0;
 
 function summary() {
   clearTimeout(profilingHandle);
-  console.log(`Memory Leak Summary(${matrix.samples} samples):`);
+  console.log(`\nMemory Summary(${matrix.samples} samples):`);
+  console.log(` Duration ${duration/1000}s`);
   console.log(` Rss(From): ${matrix.startRss}`);
   console.log(` Rss(End ): ${matrix.endRss}`);
   console.log(` Detects ${matrix.endRss - matrix.startRss} bytes`);
-  process.exit();
+  console.log('====================================================');
+  console.log(` Increasing Count  : ${matrix.increasingCount} / ${matrix.samples}`);
+  console.log(` Decreasing Count  : ${matrix.decreasingCount} / ${matrix.samples}`);
+  console.log(` Increasing Rate   : ${parseInt((matrix.increasingCount / matrix.samples) * 100)}%`);
+  console.log(` Decreasing Rate   : ${parseInt((matrix.decreasingCount / matrix.samples) * 100)}%`);
+  setTimeout(process.exit, 500);
 }
 
 setTimeout(() => {
