25bd2bd00c0ca0bc94e654672fc8155079a11aeb
Yorkie Liu <yorkiefixer@gmail.com>
2018-07-13 01:24:41 +0800

    pipe: release iotjs_string when write complete

---
+++ test/memory/issue/issue-199/parent.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var child = require('child_process').fork(__dirname + '/child.js', {
+  env: {
+    isSubprocess: 'true',
+  },
+});
+
+child.on('message', (data) => {
+  // console.log(data.toString());
+});
