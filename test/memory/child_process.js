c1f352ea2205f5a95497756ab54b489e3b5aac7a
Yorkie Liu <yorkiefixer@gmail.com>
2018-07-12 19:16:45 +0800

    child_process: update check memory script

--- test/memory/child_process.js
+++ test/memory/child_process.js
@@ -1,13 +1,38 @@
 'use strict';
 
 var exec = require('child_process').exec;
+var startProfiling = false;
+var matrix = {
+  startRss: 0,
+  endRss: 0,
+  samples: 0,
+};
 
 (function main() {
   var rss = process.memoryUsage().rss;
   exec('ls', (err) => err && console.error(err));
   setTimeout(() => {
     var curr = process.memoryUsage().rss;
+    if (startProfiling) {
+      if (!matrix.startRss)
+        matrix.startRss = curr;
+      matrix.endRss = rss;
+      matrix.samples += 1;
+    }
     console.log(`> total: ${curr}, increase: ${curr - rss}`);
     main();
-  }, 100)
+  }, 0);
 })();
+
+function summary() {
+  console.log(`Memory Leak Summary(${matrix.samples} samples):`);
+  console.log(` Rss(From): ${matrix.startRss}`);
+  console.log(` Rss(End ): ${matrix.endRss}`);
+  console.log(` Detects ${matrix.endRss - matrix.startRss} bytes`);
+  process.exit();
+}
+
+setTimeout(() => {
+  startProfiling = true;
+  setTimeout(summary, 5 * 1000);
+}, 2 * 1000);
