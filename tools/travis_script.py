47dbe291e7b8e521047cff5648392891bb6601c9
weigang.xu <weigang.xu@rokid.com>
2018-07-16 19:03:39 +0800

    enable valgrind check for travis

--- tools/travis_script.py
+++ tools/travis_script.py
@@ -27,7 +27,6 @@ BUILDOPTIONS_SANITIZER = [
     '--compile-flag=-fno-omit-frame-pointer',
     '--jerry-cmake-param=-DFEATURE_SYSTEM_ALLOCATOR=ON',
     '--jerry-cmake-param=-DJERRY_LIBC=OFF',
-    '--no-check-valgrind',
     '--no-snapshot',
     '--profile=test/profiles/host-linux.profile',
     '--run-test=full',
