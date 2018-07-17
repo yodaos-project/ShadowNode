f7d3cf9c8527615e9c6f1ed05325e0f33870a279
Yorkie Liu <yorkiefixer@gmail.com>
2018-07-17 16:59:19 +0800

    tools: add detect es features script

---
+++ tools/detect-esfeature/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function check(script) {
+  try {
+    return (new Function('"use strict";\n' + script))() !== false; // eslint-disable-line no-new-func
+  } catch(ex) {
+    return false;
+  }
+}
+
+function run(tests) {
+  var results = {
+    __all: true
+  };
+  for (var key in tests) {
+    var test = tests[key];
+    if (!test)
+      continue;
+
+    if (typeof test === 'object') {
+      results[key] = run(test);
+      if (!results[key].__all)
+        results.__all = false;
+    } else {
+      results[key] = check(test);
+      if (!results[key])
+        results.__all = false;
+    }
+  }
+  return results;
+}
+
+exports.run = run;
+
