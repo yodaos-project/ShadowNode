eec21ead1cfdc0857ff1a75cbc388a330c3ff058
chenximin <ximin.chen@rokid.com>
2018-07-30 23:11:30 +0800

    fix cmake params

--- cmake/jerry.cmake
+++ cmake/jerry.cmake
@@ -24,7 +24,6 @@ ExternalProject_Add(hostjerry
   INSTALL_COMMAND ""
   CMAKE_ARGS
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    -DCMAKE_C_COMPILER=/usr/bin/gcc
     -DCMAKE_C_FLAGS="--std=c99"
     -DENABLE_ALL_IN_ONE=ON
     -DENABLE_STATIC_LINK=OFF
