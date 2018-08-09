d0ac64db0cb153db0025a302db0bccc009bcf708
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-09 21:59:29 +0800

    colorify

--- src/js/debug.js
+++ src/js/debug.js
@@ -1,10 +1,9 @@
-'use strict';
-
 var NODE_DEBUG = process.env.NODE_DEBUG;
 
 module.exports = function(tag) {
   function debug(str) {
-    console.info(`:${tag}: ${str}`);
+    var prefix = '\033';
+    console.info(`${tag} ${prefix}[90m${str}${prefix}[0m`);
   }
   if (NODE_DEBUG === '*') {
     return debug;
