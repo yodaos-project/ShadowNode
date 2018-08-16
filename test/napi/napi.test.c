aa693608ed610cc55482c78804837aff65d0b22f
legendecas <legendecas@gmail.com>
2018-08-16 17:27:58 +0800

    napi: null terminator on copying string value (#300)

--- test/napi/napi.test.c
+++ test/napi/napi.test.c
@@ -1,108 +1,66 @@
 #include <node_api.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "common.h"
 
 napi_value SayHello(napi_env env, napi_callback_info info) {
-  napi_status status;
-
   size_t argc = 0;
   // test if `napi_get_cb_info` tolerants NULL pointers.
-  status = napi_get_cb_info(env, info, &argc, NULL, NULL, NULL);
+  NAPI_CALL(env, napi_get_cb_info(env, info, &argc, NULL, NULL, NULL));
 
   napi_value str;
-  status = napi_create_string_utf8(env, "Hello", 5, &str);
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_create_string_utf8(env, "Hello", 5, &str));
 
   return str;
 }
 
 napi_value SayError(napi_env env, napi_callback_info info) {
-  napi_status status;
-
-  status = napi_throw_error(env, "foo", "bar");
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_throw_error(env, "foo", "bar"));
 
   return NULL;
 }
 
 napi_value StrictEquals(napi_env env, napi_callback_info info) {
-  napi_status status;
-
   size_t argc = 2;
   napi_value argv[2];
   napi_value thisArg;
   void* data;
-  status = napi_get_cb_info(env, info, &argc, argv, &thisArg, &data);
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_get_cb_info(env, info, &argc, argv, &thisArg, &data));
 
   bool result = false;
-  status = napi_strict_equals(env, argv[0], argv[1], &result);
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_strict_equals(env, argv[0], argv[1], &result));
 
   napi_value ret;
-  status = napi_get_boolean(env, result, &ret);
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_get_boolean(env, result, &ret));
 
   return ret;
 }
 
 napi_value Instanceof(napi_env env, napi_callback_info info) {
-  napi_status status;
-
   size_t argc = 2;
   napi_value argv[2];
   napi_value thisArg;
   void* data;
-  status = napi_get_cb_info(env, info, &argc, argv, &thisArg, &data);
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_get_cb_info(env, info, &argc, argv, &thisArg, &data));
 
   bool result = false;
-  status = napi_instanceof(env, argv[0], argv[1], &result);
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_instanceof(env, argv[0], argv[1], &result));
 
   napi_value ret;
-  status = napi_get_boolean(env, result, &ret);
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_get_boolean(env, result, &ret));
 
   return ret;
 }
 
 napi_value Init(napi_env env, napi_value exports) {
-  napi_status status;
-
-#define set_named_method(env, target, prop_name, handler)            \
-  do {                                                               \
-    napi_value fn;                                                   \
-    status = napi_create_function(env, NULL, 0, handler, NULL, &fn); \
-    if (status != napi_ok)                                           \
-      return NULL;                                                   \
-                                                                     \
-    status = napi_set_named_property(env, target, prop_name, fn);    \
-    if (status != napi_ok)                                           \
-      return NULL;                                                   \
-  } while (0);
-
-  set_named_method(env, exports, "sayHello", SayHello);
-  set_named_method(env, exports, "sayError", SayError);
-  set_named_method(env, exports, "strictEquals", StrictEquals);
-  set_named_method(env, exports, "instanceof", Instanceof);
+  SET_NAMED_METHOD(env, exports, "sayHello", SayHello);
+  SET_NAMED_METHOD(env, exports, "sayError", SayError);
+  SET_NAMED_METHOD(env, exports, "strictEquals", StrictEquals);
+  SET_NAMED_METHOD(env, exports, "instanceof", Instanceof);
 
   napi_value id;
-  status = napi_create_int32(env, 321, &id);
-  if (status != napi_ok)
-    return NULL;
-
-  status = napi_set_named_property(env, exports, "id", id);
-  if (status != napi_ok)
-    return NULL;
+  NAPI_CALL(env, napi_create_int32(env, 321, &id));
+  NAPI_CALL(env, napi_set_named_property(env, exports, "id", id));
 
   return exports;
 }
