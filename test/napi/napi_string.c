247991a1b2a8a8ad8b5f200e37c6316ea0022175
Wu Chengzhong <chengzhong.wu@rokid.com>
2018-08-16 11:53:52 +0800

    style: check tidy

--- test/napi/napi_string.c
+++ test/napi/napi_string.c
@@ -14,14 +14,14 @@ static napi_value TestUtf8(napi_env env, napi_callback_info info) {
   NAPI_CALL(env, napi_typeof(env, args[0], &valuetype));
 
   NAPI_ASSERT(env, valuetype == napi_string,
-      "Wrong type of argment. Expects a string.");
+              "Wrong type of argment. Expects a string.");
 
   char buffer[128];
   size_t buffer_size = 128;
   size_t copied;
 
-  NAPI_CALL(env,
-      napi_get_value_string_utf8(env, args[0], buffer, buffer_size, &copied));
+  NAPI_CALL(env, napi_get_value_string_utf8(env, args[0], buffer, buffer_size,
+                                            &copied));
 
   napi_value output;
   NAPI_CALL(env, napi_create_string_utf8(env, buffer, copied, &output));
@@ -41,14 +41,14 @@ static napi_value TestUtf8Insufficient(napi_env env, napi_callback_info info) {
   NAPI_CALL(env, napi_typeof(env, args[0], &valuetype));
 
   NAPI_ASSERT(env, valuetype == napi_string,
-      "Wrong type of argment. Expects a string.");
+              "Wrong type of argment. Expects a string.");
 
   char buffer[4];
   size_t buffer_size = 4;
   size_t copied;
 
-  NAPI_CALL(env,
-      napi_get_value_string_utf8(env, args[0], buffer, buffer_size, &copied));
+  NAPI_CALL(env, napi_get_value_string_utf8(env, args[0], buffer, buffer_size,
+                                            &copied));
 
   napi_value output;
   NAPI_CALL(env, napi_create_string_utf8(env, buffer, copied, &output));
@@ -67,7 +67,7 @@ static napi_value Utf8Length(napi_env env, napi_callback_info info) {
   NAPI_CALL(env, napi_typeof(env, args[0], &valuetype));
 
   NAPI_ASSERT(env, valuetype == napi_string,
-      "Wrong type of argment. Expects a string.");
+              "Wrong type of argment. Expects a string.");
 
   size_t length;
   NAPI_CALL(env, napi_get_value_string_utf8(env, args[0], NULL, 0, &length));
