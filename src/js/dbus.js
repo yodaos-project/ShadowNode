1b03550d1940f15977ee853ee1798c854ec7ab33
Yorkie Liu <yorkiefixer@gmail.com>
2018-08-20 15:44:59 +0800

    fix nit

--- src/js/dbus.js
+++ src/js/dbus.js
@@ -265,7 +265,11 @@ Bus.prototype.getService = function(name) {
  * @param {String} name
  */
 function Service(bus, name) {
-  this._bus = bus;
+  Object.defineProperty(this, '_bus', {
+    value: bus,
+    writable: false,
+    enumerable: false,
+  });
   this._dbus = bus.dbus;
   this._dbus.setMessageHandler(this.handleMessage.bind(this));
   this._dbus.requestName(name);
