a7621de72b24d089c355c256786e55c6acbcc222
Yazhong Liu <yorkiefixer@gmail.com>
2018-08-22 19:42:28 +0800

    dbus: refactor and add add tests (#323)
    This fixes the following problems:
    
    - moves sax in need, which is required only by introspect feature.
    - uses the single connection between service and getBus().
    - frees the some acquired callbacks when no need to use.
    - frees the string when decoding messages.
    - fixes the message reply unavailable by referencing the DBusMessage*.

---
+++ test/run_pass/test_dbus_service_and_call.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var assert = require('assert');
+var dbus = require('dbus');
+
+var myservice = dbus.registerService('session', 'org.myservice');
+var myobject = myservice.createObject('/org/myobject');
+var myiface = myobject.createInterface('test.dbus.myservice.Interface1');
+
+myiface.addMethod('test', {
+  out: [dbus.Define(String)] }, (cb) => {
+  cb(null, 'simple call');
+});
+myiface.addMethod('testWithArgs', {
+  in: [dbus.Define(String)],
+  out: [dbus.Define(String)] }, (num, cb) => {
+  cb(null, num + '!!!');
+});
+myiface.update();
+
+var bus = dbus.getBus();
+var plan = 0;
+// start to call
+bus.callMethod(
+  'org.myservice', 
+  '/org/myobject', 
+  'test.dbus.myservice.Interface1', 'test', '', [], (err, res) => {
+  assert.equal(err, null);
+  assert.equal(res, 'simple call');
+  plan += 1;
+});
+
+bus.callMethod(
+  'org.myservice', 
+  '/org/myobject', 
+  'test.dbus.myservice.Interface1', 'testWithArgs', 's', ['foobar'], (err, res) => {
+  assert.equal(err, null);
+  assert.equal(res, 'foobar!!!');
+  plan += 1;
+});
+
+setTimeout(function() {
+  assert.equal(plan, 2);
+  bus.destroy();
+}, 500);
+
