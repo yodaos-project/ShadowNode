5bfeae7cf1ee44d0c2edcfd6207ce17384bf8de5
lolBig <chhxxc@gmail.com>
2018-09-25 11:21:44 +0800

    fix test lint

--- test/run_pass/test_net_http_dns_error.js
+++ test/run_pass/test_net_http_dns_error.js
@@ -1,18 +1,19 @@
 var assert = require('assert');
 var http = require('http');
+var common = require('../common');
 
-var host = 'www.unable-to-resolved-host.com'
-var req = http.request({
-  host: host,
-})
-req.socket.on('finish', () => {
+var host = 'www.unable-to-resolved-host.rokid.com';
+var req = http.request({ host: host });
+
+req.socket.on('finish', common.mustCall(() => {
   process.nextTick(() => {
     var destroyed = req.socket._socketState.destroyed;
-    console.log(`socket is destroyed ${host}:`, destroyed)
-    assert.equal(destroyed, true)
-  })
-})
+    console.log(`socket is destroyed ${host}:`, destroyed);
+    assert.equal(destroyed, true);
+  });
+}));
+
 // handle error event to prevent the process throw an error
-req.on('error', (err) => {
+req.on('error', common.mustCall((err) => {
   console.log('request error', err.message)
-})
+}));
