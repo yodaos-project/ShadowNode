0c9a40eb6071809b66d2439fbe8107a1de0f9e7b
Jim Tang <txiaozhe@gmail.com>
2018-10-02 13:57:39 +0800

    events: emit 'removeListener' after remove listener. (#358)

---
+++ test/run_pass/test_events_remove_listener.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var EventEmitter = require('events').EventEmitter;
+var assert = require('assert');
+var common = require('../common');
+
+var bus = new EventEmitter();
+
+function noop() {}
+
+bus.on('foobar', noop);
+
+bus.on('removeListener', common.mustCall((event, fn) => {
+  assert.strictEqual(event, 'foobar');
+  assert.strictEqual(fn, noop);
+}));
+
+bus.removeListener('foobar', noop);
