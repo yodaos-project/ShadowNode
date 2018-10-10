3caa0d598aedba7a2e2dda5eb3161c28b40f0cc2
lolBig <chhxxc@gmail.com>
2018-10-10 18:13:05 +0800

    fix leak

--- test/node/parallel/test-http-abort.js
+++ test/node/parallel/test-http-abort.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var https = require('https');
 var assert = require('assert');
+var common = require('../common')
 
 function getHandle(url) {
   if (/^http:\/\//.test(url)) {
@@ -11,10 +12,19 @@ function getHandle(url) {
   throw new Error('unsupported url ' + url);
 }
 
-// test http_client
-test('http://example.com/');
-// test https_client
-test('https://example.com/');
+// // test http_client
+test('http://www.example.com/');
+// // test https_client
+test('https://www.example.com/');
+// unreachable tunnels, manually abort them
+var req1 = test('http://127.0.0.2');
+setTimeout(function () {
+  req1.abort();
+}, 1000)
+var req2 = test('https://127.0.0.2');
+setTimeout(function () {
+  req2.abort();
+}, 1000)
 
 function test(url) {
   var isAborted = false;
@@ -23,20 +33,27 @@ function test(url) {
   var handle = getHandle(url);
   var req = handle.get(url, function(res) {
     res.on('data', function(chunk) {
-      assert.strictEqual(isAborted, false, 'should not aborted');
+      console.log(`${url} ondata`)
+      assert.strictEqual(isAborted, false, `${url} should not aborted`);
       isAborted = true;
       req.abort();
       process.nextTick(function() {
-        assert.strictEqual(eventTriggered, true, 'should trigger event abort');
+        assert.strictEqual(eventTriggered, true, `${url} should trigger event abort`);
       });
       chunks.push(chunk);
     });
 
   });
 
-  req.on('abort', function() {
+  req.on('abort', common.mustCall(function() {
+    console.log(`${url} aborted`)
     eventTriggered = true;
-  });
+  }));
+
+  req.socket.on('close', common.mustCall(function() {
+    console.log(`${url} socket closed`);
+  }));
 
   req.end();
+  return req;
 }
