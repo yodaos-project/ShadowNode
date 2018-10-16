4e11ec30eea41c787598067b621a3815094c0958
Yazhong Liu <yorkiefixer@gmail.com>
2018-10-16 20:36:49 +0800

    process: add signal events support (#372)

--- src/js/child_process.js
+++ src/js/child_process.js
@@ -5,26 +5,7 @@ var EventEmitter = require('events').EventEmitter;
 var StringDecoder = require('string_decoder').StringDecoder;
 var util = require('util');
 var net = require('net');
-
-var SIGNAL_NO = {
-  SIGHUP: 1,
-  SIGINT: 2,
-  SIGQUIT: 3,
-  SIGILL: 4,
-  SIGTRAP: 5,
-  SIGABRT: 6,
-  SIGBUS: 7,
-  SIGFPE: 8,
-  SIGKILL: 9,
-  SIGUSR1: 10,
-  SIGSEGV: 11,
-  SIGUSR2: 12,
-  SIGPIPE: 13,
-  SIGALRM: 14,
-  SIGTERM: 15,
-  SIGSTKFLT: 16,
-  SIGSTOP: 19,
-};
+var SIGNAL_NO = require('constants').os.signals;
 
 function signalName(code) {
   if (!code)
