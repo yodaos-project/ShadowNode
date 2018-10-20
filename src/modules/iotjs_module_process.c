a2810b169abe07bd2bcf3c4705623815fecef190
Yazhong Liu <yorkiefixer@gmail.com>
2018-10-20 14:41:57 +0800

    Fix nits

--- src/modules/iotjs_module_process.c
+++ src/modules/iotjs_module_process.c
@@ -232,8 +232,8 @@ JS_FUNCTION(CompileModule) {
     jres = jerry_exec_snapshot_at((const void*)iotjs_js_modules_s,
                                   iotjs_js_modules_l, js_modules[i].idx, false);
 #else
-    jres = WrapEval(name, iotjs_string_size(&id),
-                    (const char*)js_modules[i].code, js_modules[i].length);
+    jres = WrapEval(name, iotjs_string_size(&id), js_modules[i].code,
+                    js_modules[i].length);
 #endif
 
     if (!jerry_value_has_error_flag(jres)) {
