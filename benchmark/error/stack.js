5ccae023f98757f7bec156ba920151a392baf8db
Yazhong Liu <yorkiefixer@gmail.com>
2018-10-26 02:27:42 +0800

    iotjs: cache parsed dump table for error.stack at runtime (#392)

---
+++ benchmark/error/stack.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var common = require('../common.js');
+var bench = common.createBenchmark(main, {
+  n: [128],
+});
+
+function test2() {
+  throw new Error('yo')
+}
+
+function test() {
+  test2()
+}
+
+function main(opts) {
+  var n = opts.n;
+  bench.start();
+  for (var i = 0; i < n; i += 1) {
+    try {
+      test()
+    } catch (err) {
+      var stack = err.stack
+    }
+  }
+  bench.end(n);
+}
