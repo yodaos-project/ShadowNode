04e933add5ad38eb2e7fdbb5e2845c4b34b490ad
chenximin <ximin.chen@rokid.com>
2018-10-27 23:29:58 +0800

    remove outdated tests

--- deps/jerry/tests/jerry/array-prototype-includes.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert')
-
-var sample = [7, 7, 7, 7];
-
-assert.sameValue(sample.includes(7, 4), false, "length");
-assert.sameValue(sample.includes(7, 5), false, "length + 1");
