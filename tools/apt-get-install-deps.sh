fc13c165d24029443338a4b3a1050b6310720c4c
legendecas <legendecas@gmail.com>
2018-10-30 13:40:32 +0800

    tools: clang-format-7 on travis-ci (#398)

--- tools/apt-get-install-deps.sh
+++ tools/apt-get-install-deps.sh
@@ -18,12 +18,25 @@ sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | \
   sudo tee /etc/apt/sources.list.d/yarn.list
 
+# Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+echo "
+deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty main
+deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty main
+deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-7 main
+deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-7 main
+" | sudo tee /etc/apt/sources.list.d/llvm.list
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
 sudo apt-get update -q
 sudo apt-get install -q -y \
-    cmake valgrind clang-format-3.8 \
+    cmake valgrind clang-format-7 \
     zlib1g-dev \
     dbus libdbus-1-dev dbus-x11 \
     mosquitto \
     build-essential
 
 sudo apt-get install -y -qq yarn
+
+sudo ln -s /usr/bin/clang-format-7 /usr/bin/clang-format-7.0
