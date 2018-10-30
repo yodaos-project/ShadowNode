4d60eb3e3a1523a21e6b50be071c9eb3a53a41ed
chenximin <ximin.chen@rokid.com>
2018-10-31 01:11:59 +0800

    fix lint

--- src/js/next_tick.js
+++ src/js/next_tick.js
@@ -13,22 +13,25 @@ function NextTickQueue() {
 
 NextTickQueue.prototype.push = function push(v) {
   var entry = { data: v, next: null };
-  if (this.length > 0)
+  if (this.length > 0) {
     this.tail.next = entry;
-  else
+  } else {
     this.head = entry;
+  }
   this.tail = entry;
   ++this.length;
 };
 
 NextTickQueue.prototype.shift = function shift() {
-  if (this.length === 0)
+  if (this.length === 0) {
     return;
+  }
   var ret = this.head.data;
-  if (this.length === 1)
+  if (this.length === 1) {
     this.head = this.tail = null;
-  else
+  } else {
     this.head = this.head.next;
+  }
   --this.length;
   return ret;
 };
@@ -50,8 +53,10 @@ module.exports.nextTick = function nextTick(callback) {
     case 4: args = [arguments[1], arguments[2], arguments[3]]; break;
     default:
       args = new Array(arguments.length - 1);
-      for (var i = 1; i < arguments.length; i++)
+      for (var i = 1; i < arguments.length; i++) {
         args[i - 1] = arguments[i];
+      }
+      break;
   }
   var tickObject = new TickObject(callback, args);
   nextTickQueue.push(tickObject);
@@ -66,17 +71,10 @@ module.exports._onNextTick = function _onNextTick() {
       callback();
     } else {
       switch (args.length) {
-        case 1:
-          callback(args[0]);
-          break;
-        case 2:
-          callback(args[0], args[1]);
-          break;
-        case 3:
-          callback(args[0], args[1], args[2]);
-          break;
-        default:
-          callback.apply(undefined, args);
+        case 1: callback(args[0]); break;
+        case 2: callback(args[0], args[1]); break;
+        case 3: callback(args[0], args[1], args[2]); break;
+        default: callback.apply(undefined, args); break;
       }
     }
   }
