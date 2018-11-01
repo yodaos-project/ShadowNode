654860b4efd5921d0f6871aa3138ea23300ab09c
chenximin <ximin.chen@rokid.com>
2018-11-01 15:59:35 +0800

    update test case

--- test/run_pass/test_mqtt_client.js
+++ test/run_pass/test_mqtt_client.js
@@ -1,22 +1,32 @@
 'use strict';
 
-var testHost = 'mqtt://test.mosquitto.org:1883';
+var mqttHost = 'mqtt://test.mosquitto.org:1883';
+var mqttsHost = 'mqtts://test.mosquitto.org:8883';
 var mqtt = require('mqtt');
 var assert = require('assert');
 var common = require('../common');
 
-var client = mqtt.connect(testHost, {
-  reconnectPeriod: -1
-});
-// testHost may not be able to connect, so don't use mustCall here
-client.once('connect', function() {
-  // trigger the close event
-  client.disconnect();
-  assert.equal(client._keepAliveTimer, null);
-  assert.equal(client._keepAliveTimeout, null);
-});
-//the close event will be triggered whether testHost is connected or not
-client.once('close', common.mustCall(function() {
-  assert.ok(!client.connected);
-}));
+function test(testHost) {
+  var client = mqtt.connect(testHost, {
+    reconnectPeriod: -1
+  });
+  // testHost may not be able to connect, so don't use mustCall here
+  client.once('connect', common.mustCall(function() {
+    // trigger the close event
+    client.once('offline', common.mustCall(function() {
+      assert.ok(!client.connected);
+      assert.ok(!client._isTCPConnected);
+    }));
+    client.disconnect();
+    assert.equal(client._keepAliveTimer, null);
+    assert.equal(client._keepAliveTimeout, null);
+  }));
+  //the close event will be triggered whether testHost is connected or not
+  client.once('close', common.mustCall(function() {
+    assert.ok(!client.connected);
+    assert.ok(!client._isTCPConnected);
+  }));
+}
 
+test(mqttHost);
+test(mqttsHost);
