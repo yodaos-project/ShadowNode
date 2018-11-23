abd0f73a629923a4a411a20da5b3a9809ee4665b
Txiaozhe <txiaozhe@gmail.com>
2018-11-23 22:07:33 +0800

    test this api

--- test/run_pass/test_os_network_interfaces.js
+++ test/run_pass/test_os_network_interfaces.js
@@ -1,5 +1,21 @@
 'use strict';
 
+var assert = require('assert');
 var os = require('os');
 
-console.log(os.networkInterfaces());
+var family = {
+  IPv4: 1,
+  IPv6: 1
+};
+
+var net_info = os.networkInterfaces();
+
+for (var net_name in net_info) {
+  net_info[net_name].forEach(net => {
+    assert.equal(typeof net.address, 'string');
+    assert.equal(typeof net.netmask, 'string');
+    assert.equal(family[net.family], 1);
+    assert.equal(typeof net.broadcast, 'string');
+    assert.equal(typeof net.mac, 'string');
+  });
+}
