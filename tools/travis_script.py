yodaos-project/ShadowNode

commit d68d75e657a67275768d15fa80d12fe09a61a5e6

jerry: pass and enable jerry-test-suite (#425)

tools/travis_script.py
@@ -8,8 +8,21 @@ from common_py.system.executor import Executor as ex
 BUILDTYPES = ['debug', 'release']
 
 
+def check_change(path):
+    '''Check if current pull request depends on path.
+    Return -1 if not depends, else if depends.'''
+    commit_range = os.getenv('TRAVIS_COMMIT_RANGE').partition('...')
+    commit_head = commit_range[0]
+    commit_base = commit_range[2]
+    commit_diff = ex.run_cmd_output('git',
+                                    ['diff', commit_head, commit_base], True)
+    return commit_diff.find(path)
+
+
 def build_jerry():
-    ex.check_run_cmd('./deps/jerry/tools/run-tests.py', ['--unittests'])
+    if check_change('deps/jerry') != -1:
+        ex.check_run_cmd('./deps/jerry/tools/run-tests.py',
+                         ['--unittests', '--jerry-test-suite'])
 
 
 def build_iotjs(buildtype, args=[], env=[]):

deps/jerry/jerry-core/api/jerry.c
@@ -2958,6 +2958,7 @@ jerry_create_arraybuffer_external (const jerry_length_t size, /**< size of the b
 #else /* CONFIG_DISABLE_ES2015_TYPEDARRAY_BUILTIN */
   JERRY_UNUSED (size);
   JERRY_UNUSED (buffer_p);
+  JERRY_UNUSED (free_hint);
   JERRY_UNUSED (free_cb);
   return jerry_throw (ecma_raise_type_error (ECMA_ERR_MSG ("ArrayBuffer not supported.")));
 #endif /* !CONFIG_DISABLE_ES2015_TYPEDARRAY_BUILTIN */

deps/jerry/jerry-core/ecma/builtin-objects/ecma-builtins.c
@@ -805,10 +805,17 @@ ecma_builtin_try_to_instantiate_property (ecma_object_t *object_p, /**< object *
          *  So routine id are not consist to declaration in their inc.h.
          *  Magic string id of name should be set correctly.
          */
-        JERRY_ASSERT (builtin_id ==ECMA_BUILTIN_ID_DATE_PROTOTYPE || builtin_id == ECMA_BUILTIN_ID_MATH );
+#if (!defined CONFIG_DISABLE_DATE_BUILTIN) && (!defined CONFIG_DISABLE_MATH_BUILTIN)
+        JERRY_ASSERT (builtin_id == ECMA_BUILTIN_ID_DATE_PROTOTYPE || builtin_id == ECMA_BUILTIN_ID_MATH);
+#elif (defined CONFIG_DISABLE_DATE_BUILTIN) && (!defined CONFIG_DISABLE_MATH_BUILTIN)
+        JERRY_ASSERT (builtin_id == ECMA_BUILTIN_ID_MATH);
+#elif (!defined CONFIG_DISABLE_DATE_BUILTIN) && (defined CONFIG_DISABLE_MATH_BUILTIN)
+        JERRY_ASSERT (builtin_id == ECMA_BUILTIN_ID_DATE_PROTOTYPE);
+#endif
+
         ecma_builtin_routine_set_name (builtin_id,
-                                                                    ECMA_GET_ROUTINE_ID (curr_property_p->value),
-                                                                    magic_string_id);
+                                       ECMA_GET_ROUTINE_ID (curr_property_p->value),
+                                       magic_string_id);
       }
       value = ecma_make_object_value (func_obj_p);
       break;

commit 64fe53935646adf171c40bad9b93e828d968dc39

test: fix wrong travis diff target introduced by #425

tools/travis_script.py
@@ -11,11 +11,13 @@ BUILDTYPES = ['debug', 'release']
 def check_change(path):
     '''Check if current pull request depends on path.
     Return -1 if not depends, else if depends.'''
-    commit_range = os.getenv('TRAVIS_COMMIT_RANGE').partition('...')
-    commit_head = commit_range[0]
-    commit_base = commit_range[2]
+    travis_branch = os.getenv('TRAVIS_BRANCH')
     commit_diff = ex.run_cmd_output('git',
-                                    ['diff', commit_head, commit_base], True)
+                                    [
+                                        'diff',
+                                        '--name-only',
+                                        'HEAD...' + travis_branch],
+                                    True)
     return commit_diff.find(path)
 
 