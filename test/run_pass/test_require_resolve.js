e1904b84002d75f5271415f43d92e14563088889
Txiaozhe <txiaozhe@gmail.com>
2018-12-18 13:11:16 +0800

    leading by ./

--- test/run_pass/test_require_resolve.js
+++ test/run_pass/test_require_resolve.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 var fixtures = require('../common/fixtures');
 
 assert.strictEqual(
-  require.resolve('test_util').toLowerCase(),
+  require.resolve('./test_util').toLowerCase(),
   fixtures.path('./test/run_pass/test_util.js').toLowerCase());
 
 assert.strictEqual(
