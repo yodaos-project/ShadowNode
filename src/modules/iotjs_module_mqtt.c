32ccb1b5d12e55e79059336e376706ecc1d2a9ad
Yorkie Liu <yorkiefixer@gmail.com>
2018-12-23 00:56:59 +0800

    fix style nits

--- src/modules/iotjs_module_mqtt.c
+++ src/modules/iotjs_module_mqtt.c
@@ -42,8 +42,7 @@ JS_FUNCTION(MqttConstructor) {
       iotjs_jval_get_property(opts, IOTJS_MAGIC_STRING_PASSWORD);
   jerry_value_t clientID =
       iotjs_jval_get_property(opts, IOTJS_MAGIC_STRING_CLIENT_ID);
-  jerry_value_t will =
-      iotjs_jval_get_property(opts, IOTJS_MAGIC_STRING_WILL);
+  jerry_value_t will = iotjs_jval_get_property(opts, IOTJS_MAGIC_STRING_WILL);
 
   MQTTPacket_connectData options = MQTTPacket_connectData_initializer;
   options.willFlag = 0;
@@ -51,16 +50,17 @@ JS_FUNCTION(MqttConstructor) {
   options.keepAliveInterval = (int)iotjs_jval_as_number(keepalive);
   options.cleansession = 1;
 
-#define MQTT_OPTION_ASSIGN_FROM(owner, name) do { \
-  if (jerry_value_is_string(name)) { \
-    iotjs_string_t str = iotjs_jval_as_string(name); \
-    MQTTString mqttstr = MQTTString_initializer; \
-    mqttstr.lenstring.data = strdup((char*)iotjs_string_data(&str)); \
-    mqttstr.lenstring.len = (int)iotjs_string_size(&str); \
-    owner.name = mqttstr; \
-    iotjs_string_destroy(&str); \
-  } \
-} while(0)
+#define MQTT_OPTION_ASSIGN_FROM(owner, name)                           \
+  do {                                                                 \
+    if (jerry_value_is_string(name)) {                                 \
+      iotjs_string_t str = iotjs_jval_as_string(name);                 \
+      MQTTString mqttstr = MQTTString_initializer;                     \
+      mqttstr.lenstring.data = strdup((char*)iotjs_string_data(&str)); \
+      mqttstr.lenstring.len = (int)iotjs_string_size(&str);            \
+      owner.name = mqttstr;                                            \
+      iotjs_string_destroy(&str);                                      \
+    }                                                                  \
+  } while (0)
 
   MQTT_OPTION_ASSIGN_FROM(options, username);
   MQTT_OPTION_ASSIGN_FROM(options, password);
@@ -68,9 +68,12 @@ JS_FUNCTION(MqttConstructor) {
 
   if (jerry_value_is_object(will)) {
     MQTTPacket_willOptions willOpts = MQTTPacket_willOptions_initializer;
-    jerry_value_t topicName = iotjs_jval_get_property(will, IOTJS_MAGIC_STRING_TOPIC);
-    jerry_value_t message = iotjs_jval_get_property(will, IOTJS_MAGIC_STRING_PAYLOAD);
-    jerry_value_t retained = iotjs_jval_get_property(will, IOTJS_MAGIC_STRING_RETAIN);
+    jerry_value_t topicName =
+        iotjs_jval_get_property(will, IOTJS_MAGIC_STRING_TOPIC);
+    jerry_value_t message =
+        iotjs_jval_get_property(will, IOTJS_MAGIC_STRING_PAYLOAD);
+    jerry_value_t retained =
+        iotjs_jval_get_property(will, IOTJS_MAGIC_STRING_RETAIN);
     jerry_value_t qos = iotjs_jval_get_property(will, IOTJS_MAGIC_STRING_QOS);
     willOpts.retained = iotjs_jval_as_boolean(retained);
     willOpts.qos = iotjs_jval_as_number(qos);
