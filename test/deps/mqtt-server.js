702501465abac6355052be7156c247619fd5cff0
Yazhong Liu <yorkiefixer@gmail.com>
2018-12-25 21:11:31 +0800

    mqtt: support `will` options for sending messages while being disconnected (#453)

--- test/deps/mqtt-server.js
+++ test/deps/mqtt-server.js
@@ -37,7 +37,7 @@ server.on('clientConnected', function(client) {
 
 // fired when a message is received
 server.on('published', function(packet, client) {
-  console.log('Published', packet.payload);
+  console.log('Published', packet);
 });
 
 server.on('ready', function() {
