90e373a710b0e3f5178ea079c78181ae4340bea3
ErosZy <zyeros1991@gmail.com>
2018-12-26 11:04:20 +0800

    style: eof

--- deps/jerry/tests/jerry/number-is.js
+++ deps/jerry/tests/jerry/number-is.js
@@ -1,5 +1,5 @@
-function assertTrue(pred){ assert(pred) }
-function assertFalse(pred){ assert(!pred) }
+function assertTrue(pred) { assert(pred); }
+function assertFalse(pred) { assert(!pred); }
 
 assertTrue(Number.isFinite(0));
 assertTrue(Number.isFinite(Number.MIN_VALUE));
@@ -8,11 +8,11 @@ assertFalse(Number.isFinite(Number.NaN));
 assertFalse(Number.isFinite(Number.POSITIVE_INFINITY));
 assertFalse(Number.isFinite(Number.NEGATIVE_INFINITY));
 assertFalse(Number.isFinite(new Number(0)));
-assertFalse(Number.isFinite(1/0));
-assertFalse(Number.isFinite(-1/0));
+assertFalse(Number.isFinite(1 / 0));
+assertFalse(Number.isFinite(-1 / 0));
 assertFalse(Number.isFinite({}));
 assertFalse(Number.isFinite([]));
-assertFalse(Number.isFinite("s"));
+assertFalse(Number.isFinite('s'));
 assertFalse(Number.isFinite(null));
 assertFalse(Number.isFinite(undefined));
 
@@ -23,17 +23,17 @@ assertTrue(Number.isNaN(Number.NaN));
 assertFalse(Number.isNaN(Number.POSITIVE_INFINITY));
 assertFalse(Number.isNaN(Number.NEGATIVE_INFINITY));
 assertFalse(Number.isNaN(new Number(0)));
-assertFalse(Number.isNaN(1/0));
-assertFalse(Number.isNaN(-1/0));
+assertFalse(Number.isNaN(1 / 0));
+assertFalse(Number.isNaN(-1 / 0));
 assertFalse(Number.isNaN({}));
 assertFalse(Number.isNaN([]));
-assertFalse(Number.isNaN("s"));
+assertFalse(Number.isNaN('s'));
 assertFalse(Number.isNaN(null));
 assertFalse(Number.isNaN(undefined));
 
 assertFalse(Number.isInteger({}));
 assertFalse(Number.isInteger([]));
-assertFalse(Number.isInteger("s"));
+assertFalse(Number.isInteger('s'));
 assertFalse(Number.isInteger(null));
 assertFalse(Number.isInteger(undefined));
 assertFalse(Number.isInteger(new Number(2)));
@@ -43,12 +43,12 @@ assertTrue(Number.isInteger(Number.MAX_VALUE));
 assertFalse(Number.isInteger(Number.NaN));
 assertFalse(Number.isInteger(Number.POSITIVE_INFINITY));
 assertFalse(Number.isInteger(Number.NEGATIVE_INFINITY));
-assertFalse(Number.isInteger(1/0));
-assertFalse(Number.isInteger(-1/0));
+assertFalse(Number.isInteger(1 / 0));
+assertFalse(Number.isInteger(-1 / 0));
 
 assertFalse(Number.isSafeInteger({}));
 assertFalse(Number.isSafeInteger([]));
-assertFalse(Number.isSafeInteger("s"));
+assertFalse(Number.isSafeInteger('s'));
 assertFalse(Number.isSafeInteger(null));
 assertFalse(Number.isSafeInteger(undefined));
 assertFalse(Number.isSafeInteger(new Number(2)));
@@ -58,8 +58,8 @@ assertFalse(Number.isSafeInteger(Number.MAX_VALUE));
 assertFalse(Number.isSafeInteger(Number.NaN));
 assertFalse(Number.isSafeInteger(Number.POSITIVE_INFINITY));
 assertFalse(Number.isSafeInteger(Number.NEGATIVE_INFINITY));
-assertFalse(Number.isSafeInteger(1/0));
-assertFalse(Number.isSafeInteger(-1/0));
+assertFalse(Number.isSafeInteger(1 / 0));
+assertFalse(Number.isSafeInteger(-1 / 0));
 
 var near_upper = Math.pow(2, 52);
 assertTrue(Number.isSafeInteger(near_upper));
