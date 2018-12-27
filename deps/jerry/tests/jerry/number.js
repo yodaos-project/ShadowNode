323a86914f4e0a5b625ec60c2ee33d449a15dce1
ErosZy <zyeros1991@gmail.com>
2018-12-27 22:01:20 +0800

    format code style

--- deps/jerry/tests/jerry/number.js
+++ deps/jerry/tests/jerry/number.js
@@ -9,13 +9,13 @@ function assertThrow(pred) {
 }
 
 function assertNotThrow(pred) {
-    var isNotThrow = true;
-    try {
-      eval(pred);
-    } catch (e) {
-        isNotThrow = false;
-    }
-    assert(isNotThrow);
+  var isNotThrow = true;
+  try {
+    eval(pred);
+  } catch (e) {
+    isNotThrow = false;
+  }
+  assert(isNotThrow);
 }
 
 assert(0xbf == 191);
@@ -26,7 +26,7 @@ assertNotThrow('0xbf;');
 assertNotThrow('i = 0xbf');
 
 assert(0o37 == 31);
-assert(0O37 == 31);
+assert(0o37 == 31);
 assert(0o37 - 10 == 21);
 assertThrow('0o38');
 assertThrow('0O38');
@@ -35,7 +35,7 @@ assertNotThrow('0O37;');
 assertNotThrow('i = 0o37');
 
 assert(0b101 == 5);
-assert(0B101 == 5);
+assert(0b101 == 5);
 assert(0b101 - 2 == 3);
 assertThrow('0b12');
 assertThrow('0B12');
