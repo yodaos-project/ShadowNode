2fd541654b8cc2994492a945c6f6c3e6aa78cc78
Txiaozhe <txiaozhe@gmail.com>
2019-01-04 19:51:54 +0800

    check read error

--- src/modules/iotjs_module_process.c
+++ src/modules/iotjs_module_process.c
@@ -146,11 +146,12 @@ JS_FUNCTION(Compile) {
 
   size_t size = 0;
   char* source = iotjs__file_read(filename, &size);
-  jerry_value_t jres = WrapEval(filename, strlen(filename), source, size);
-  if (source == NULL || size == 0) {
-    return jres;
+  if (source == NULL) {
+    iotjs_string_destroy(&path);
+    return JS_CREATE_ERROR(COMMON, "Could not load the source.");
   }
 
+  jerry_value_t jres = WrapEval(filename, strlen(filename), source, size);
   iotjs_string_destroy(&path);
   iotjs_buffer_release(source);
   return jres;
