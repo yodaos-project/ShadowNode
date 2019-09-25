e3b787ae603544e661082f259c238de485ead406
yorkie <yorkiefixer@gmail.com>
2019-09-25 20:55:31 +0800

    fixup

--- test/run_pass/test_fs_write_stream.js
+++ test/run_pass/test_fs_write_stream.js
@@ -10,9 +10,11 @@ var originalContents = fs.readFileSync(dstFilePath);
 var buff1 = Buffer.from('IoT');
 var buff2 = Buffer.from('.js');
 
+function nop() {}
+
 var stream = fs.createWriteStream(dstFilePath);
-stream.write(buff1);
-stream.write(buff2);
+stream.write(buff1, nop);
+stream.write(buff2, nop);
 stream.on('finish', common.mustCall(function onend() {
   var outputs = fs.readFileSync(dstFilePath, 'utf8');
   assert.strictEqual(outputs, 'IoT.js');
